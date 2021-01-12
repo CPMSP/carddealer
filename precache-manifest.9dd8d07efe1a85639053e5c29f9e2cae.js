f8350eea5dd67e88c86d2f11c372b67c94bf31e4
CPMSP <cpetersonmsp@gmail.com>
2021-01-11 22:44:17 -0600

    Updates

---
+++ precache-manifest.9dd8d07efe1a85639053e5c29f9e2cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38006b24d8e947017e216239f5aa1f83",
+    "url": "/carddealer/index.html"
+  },
+  {
+    "revision": "eb253e00ed69721c0681",
+    "url": "/carddealer/static/css/main.fa8b6693.chunk.css"
+  },
+  {
+    "revision": "3ed81e055cfdb98916a1",
+    "url": "/carddealer/static/js/2.971d2a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carddealer/static/js/2.971d2a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb253e00ed69721c0681",
+    "url": "/carddealer/static/js/main.7991a01d.chunk.js"
+  },
+  {
+    "revision": "176fba26db262268d389",
+    "url": "/carddealer/static/js/runtime-main.15cee17c.js"
+  }
+]);
